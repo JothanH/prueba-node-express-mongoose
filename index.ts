32579b0efcc2e5fa345ed05647b28b696508b4b9
JothanH <jonathan.henao@gmail.com>
2021-07-12 17:55:29 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,53 @@
+import Server from "./classes/server";
+import testRoutes from "./routes/test";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import express from 'express';
+import colors from "colors";
+
+
+const server = new Server();
+colors.enable();
+
+//Body parser
+server.app.use(express.urlencoded({ extended : true }));
+server.app.use(express.json());
+
+//Rutas de la app
+server.app.use('/test', testRoutes);
+
+//Conexión  de Base de datos
+const URI = 'mongodb://localhost:27017/sennova-test';
+
+let options: mongoose.ConnectOptions = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+};
+
+mongoose.connect(URI, options, (err) => {
+
+    if(err) {
+        console.log("\n");
+        console.log("****************************".bgRed);
+        console.log("Mongo no se pudo conectar".red);
+        console.log("****************************".bgRed);
+        console.log("\n");
+        console.log(err);
+
+        throw err;
+    }
+    console.log("\n");
+    console.log("*****************************".bgGreen);
+    console.log("Mongo conectado correctamente".italic.green);
+    console.log("*****************************".bgGreen);
+    console.log("\n");
+});
+
+
+//Levantar express
+server.start(() => {
+    console.log("\n");
+    console.log(`Servidor corriendo el puerto ${ server.port}`.italic.grey);
+
+});
---
+++ routes/test.ts
@@ -0,0 +1,94 @@
+import {Router, Request, Response} from "express";
+import { Test } from "../models/test.model";
+
+
+const testRoutes = Router();
+
+
+testRoutes.post('/datos', (req: Request, res: Response) => {
+
+    const test = {
+        nombre: req.body.nombre,
+        cantidad: req.body.cantidad,
+        activo: req.body.activo
+    }
+
+    Test.create(test).then( testDb => {
+
+        res.json({
+            ok: true,
+            test: testDb
+        });
+
+    }).catch(err => {
+
+        res.json({
+            ok:false,
+            error: err
+        });
+    });
+});
+
+testRoutes.get('/datos', (req: Request, res: Response) => {
+
+    Test.find().then(datos => {
+
+        res.json({
+            ok: true,
+            mensaje:'datos traídos con éxito',
+            datos: datos
+        });
+    });
+
+});
+
+testRoutes.put('/datos', (req: Request, res: Response) => {
+
+    const test = {
+        id: req.body.id,
+        nombre: req.body.nombre,
+        cantidad: req.body.cantidad,
+        activo: req.body.activo
+    }
+
+    Test.findByIdAndUpdate({_id: test.id}, {
+        nombre: test.nombre,
+        cantidad: test.cantidad,
+        activo: test.activo
+    }).then( answ => {
+        res.json({
+            ok: true,
+            mensaje:'datos actualizados con éxito'
+        });
+    }).catch(err => {
+
+        res.json({
+            ok:false,
+            error: err
+        });
+    });
+});
+
+testRoutes.delete('/datos/:id', (req: Request, res: Response) => {
+
+
+    const id = req.params.id;
+
+
+    Test.findByIdAndDelete(id).then(answ => {
+
+        res.json({
+            ok: true,
+            mensaje:'datos borrados con éxito'
+        });
+    }).catch(err => {
+        res.json({
+            ok:false,
+            error: err
+        });
+    });
+
+});
+
+
+export default testRoutes;
